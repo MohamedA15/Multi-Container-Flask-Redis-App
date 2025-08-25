29e2d1d68d12f024baa55791b096caf7849aa2d4
MohamedA15 <mo684073@gmail.com>
2025-08-25 14:39:19 +0100

    Updated webapp

--- app.py
+++ app.py
@@ -1,24 +1,29 @@
-# app.py
-
-from flask import Flask
+from flask import Flask, render_template
 import redis
 
-# Create Flask app
 app = Flask(__name__)
 
 # Connect to Redis container
-r = redis.Redis(host='redis', port=6379)
+r = redis.Redis(host='redis', port=6379, decode_responses=True)
 
+# Reset counter at startup
+r.set('visits', 0)
 
 @app.route('/')
 def hello_welcome():
-    return "Hello, welcome to this Coderco web!"
+    # Show current count without incrementing
+    count = r.get('visits')
+    if count is None:
+        count = 0
+    else:
+        count = int(count)
+    return render_template('index.html', count=count)
 
 @app.route('/count')
-def count():
+def count_page():
     # Increment the counter in Redis
     count = r.incr('visits')
-    return f'This page has been visited {count} times.'
+    return render_template('index.html', count=count)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5001)
+    app.run(host='0.0.0.0', port=5001, debug=True)
